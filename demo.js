dce1fd6bbbb3aace9b21a19cb332c1c8a2b9bc9c
Christoph Reiter <reiter.christoph@gmail.com>
2021-04-28 17:21:20 +0200

    Add an internal wc so it's doing something

--- demo.js
+++ demo.js
@@ -1,7 +1,17 @@
 // uncomment to make things work
-//import 'https://unpkg.com/@open-wc/scoped-elements@2.0.0-next.0/src/loadPolyfill.js';
+// import 'https://unpkg.com/@open-wc/scoped-elements@2.0.0-next.0/src/loadPolyfill.js';
 import {LitElement, html} from 'lit';
 import {ScopedElementsMixin} from '@open-wc/scoped-elements';
+
+class Internal extends ScopedElementsMixin(LitElement) {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return html`world`;
+    }
+}
 
 class DbpCommonDemo extends ScopedElementsMixin(LitElement) {
     constructor() {
@@ -10,11 +20,12 @@ class DbpCommonDemo extends ScopedElementsMixin(LitElement) {
 
     static get scopedElements() {
         return {
+            'something-internal': Internal,
         };
     }
 
     render() {
-        return html`hello`;
+        return html`hello <something-internal></something-internal>`;
     }
 }
 
